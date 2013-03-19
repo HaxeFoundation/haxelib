c3f6b7ce54f0032730cb93a3c12e04bec8c6b475
Philipp Klose <me@thehippo.de>
2013-03-19 12:44:06 +0100

    refractor, minor coding style improvements

--- testing/PrepareServer.hx
+++ testing/PrepareServer.hx
@@ -1,6 +1,16 @@
 package;
 
+import haxe.crypto.Crc32;
+import haxe.io.BytesInput;
+import haxe.remoting.HttpConnection;
+import haxe.zip.Entry;
+import haxe.zip.Writer;
+import sys.db.Sqlite;
 import sys.FileSystem;
+import sys.io.File;
+import tools.haxelib.SiteDb;
+
+using StringTools;
 
 class SiteProxy extends haxe.remoting.Proxy<tools.haxelib.SiteApi> {
 }
@@ -41,16 +51,16 @@ class PrepareServer {
 		var fileName = path + '.zip';
 		if (FileSystem.exists(fileName))
 			FileSystem.deleteFile(fileName);
-		var output = sys.io.File.write(path + '.zip', true);
-		var writer = new haxe.zip.Writer(output);
-		var entries = new List<haxe.zip.Entry>();
+		var output = File.write(path + '.zip', true);
+		var writer = new Writer(output);
+		var entries = new List<Entry>();
 		for (file in files) {
-			var data = sys.io.File.getBytes(path + '/' + file);
+			var data = File.getBytes(path + '/' + file);
 			entries.add({
 				fileTime: Date.now(),
 				compressed: false,
 				data: data,
-				crc32: haxe.crypto.Crc32.make(data),
+				crc32: Crc32.make(data),
 				dataSize: data.length,
 				fileName: file,
 				fileSize: 0
@@ -65,21 +75,21 @@ class PrepareServer {
 	static function main() {
 		Sys.println("Cleaning up folders");
 		var serverPath = Sys.getCwd() + 'server/';
-		if (FileSystem.exists(serverPath+ 'files') && FileSystem.isDirectory(serverPath+ 'files'))
-			deleteFolderContent(serverPath+ 'files');
-		if (FileSystem.exists(serverPath+ 'tmp') && FileSystem.isDirectory(serverPath+ 'tmp'))
-		deleteFolderContent(serverPath+ 'tmp');
+		if (FileSystem.exists(serverPath + 'files') && FileSystem.isDirectory(serverPath + 'files'))
+			deleteFolderContent(serverPath + 'files');
+		if (FileSystem.exists(serverPath + 'tmp') && FileSystem.isDirectory(serverPath + 'tmp'))
+		deleteFolderContent(serverPath + 'tmp');
 		
 		Sys.println("Cleaning DB");
 		var sqlFile = serverPath + 'haxelib.db';
-		var con = sys.db.Sqlite.open(sqlFile);
-		tools.haxelib.SiteDb.create(con);
+		var con = Sqlite.open(sqlFile);
+		SiteDb.create(con);
 
 		Sys.println("Package testing libraries");
 		var libraries = new List<String>();
 		var testFolder = Sys.getCwd() + 'testing/';
 		for (file in FileSystem.readDirectory(testFolder + 'libraries')) {
-			if (FileSystem.isDirectory(testFolder + 'libraries/' + file) && StringTools.startsWith(file, 'lib')) {
+			if (FileSystem.isDirectory(testFolder + 'libraries/' + file) && file.startsWith('lib')) {
 				packageLibrary(testFolder + 'libraries/' + file);
 				Sys.println("Packaged libray: " + file.substr(3));
 				libraries.push(file.substr(3));
@@ -87,16 +97,16 @@ class PrepareServer {
 		}
 
 		Sys.println("Submitting test libraries");
-		var site = new SiteProxy(haxe.remoting.HttpConnection.urlConnect('http://localhost:2000/').api);
+		var site = new SiteProxy(HttpConnection.urlConnect('http://localhost:2000/').api);
 		for (library in libraries) {
 			Sys.println("Submitting library: " + library);
 			site.register(library, library, library+'@example.org', library + ' Developer');
 			// TODO: This is not how haxelib usually behave, do we need dependencies checks and all the other stuff?
 			site.checkDeveloper(library,library);
-			var data = sys.io.File.getBytes(testFolder + 'libraries/lib' + library + '.zip');
+			var data = File.getBytes(testFolder + 'libraries/lib' + library + '.zip');
 			var id = site.getSubmitId();
 			var h = new haxe.Http('http://localhost:2000/');
-			h.fileTransfert("file",id,new haxe.io.BytesInput(data),data.length);
+			h.fileTransfert("file",id,new BytesInput(data),data.length);
 			h.request(true);
 			site.processSubmit(id,library,library);
 		}
