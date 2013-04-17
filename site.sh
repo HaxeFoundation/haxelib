eaf1f6bd8bcf579d1485211e06b3354b33a56c6b
Jason O'Neil <jason.oneil@gmail.com>
2013-04-17 11:10:26 +0800

    Fixes to get site setting up correctly and compiling

--- site.sh
+++ site.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Build the site, copy components over
-haxe site.hxml
 
 mkdir -p server/legacy
 mkdir -p server/api/2.0
+
+haxe site.hxml
 
 cp src/tools/haxelib/.htaccess server/
 cp src/tools/haxelib/website.mtt server/
@@ -13,6 +14,27 @@ cp src/tools/haxelib/haxelib.css server/
 cp src/tools/legacyhaxelib/.htaccess server/legacy/
 cp src/tools/legacyhaxelib/website.mtt server/legacy/
 cp src/tools/legacyhaxelib/haxelib.css server/legacy/
+
+# Make sure the server folders are writeable.  
+
+chmod o+w server/
+chmod o+w server/legacy/
+
+# If the databases don't exist, run "setup"
+
+if [ ! -f server/haxelib.db ];
+then
+    cd server
+    neko index.n setup
+    cd ..
+fi
+
+if [ ! -f server/legacy/haxelib.db ];
+then
+    cd server/legacy
+    neko index.n setup
+    cd ../..
+fi
 
 cd server
 # starting server on port 2000, because binding port 80 requires root privileges,
