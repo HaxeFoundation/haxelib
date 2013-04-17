df9dffc03b08ec181192fef3eb42c36059bee646
Jason O'Neil <jason.oneil@gmail.com>
2013-04-17 10:48:44 +0800

    Add compiling for legacy.  Add commands to site.sh to set up server/ folder

--- site.sh
+++ site.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+# Build the site, copy components over
+haxe site.hxml
+
+mkdir -p server/legacy
+mkdir -p server/api/2.0
+
+cp src/tools/haxelib/.htaccess server/
+cp src/tools/haxelib/website.mtt server/
+cp src/tools/haxelib/haxelib.css server/
+
+cp src/tools/legacyhaxelib/.htaccess server/legacy/
+cp src/tools/legacyhaxelib/website.mtt server/legacy/
+cp src/tools/legacyhaxelib/haxelib.css server/legacy/
+
 cd server
 # starting server on port 2000, because binding port 80 requires root privileges,
 # which might be a bad idea
