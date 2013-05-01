7a80b43ac9bc1b6393d7f6336b40bfd5b6574885
Jason O'Neil <jason.oneil@gmail.com>
2013-05-01 22:12:47 +0800

    Change to point to my test server for now...

--- src/tools/legacyhaxelib/Main.hx
+++ src/tools/legacyhaxelib/Main.hx
@@ -115,7 +115,7 @@ class Main {
 	static var VERSION = 104;
 	static var REPNAME = "lib";
 	static var SERVER = {
-		host : "lib.haxe.org",
+		host : "haxelib.jasononeil.com.au",
 		port : 80,
 		dir : "",
 		url : "index.n"
