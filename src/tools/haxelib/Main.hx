b14506e8370f0e7e0f4a2b563dec5db54c22ff96
Jason O'Neil <jason.oneil@gmail.com>
2013-05-07 22:05:31 +0800

    Add packaging shell script, bumped version number

--- src/tools/haxelib/Main.hx
+++ src/tools/haxelib/Main.hx
@@ -117,7 +117,7 @@ class ProgressIn extends haxe.io.Input {
 
 class Main {
 
-	static var VERSION = SemVer.ofString('3.0.0-rc.2');
+	static var VERSION = SemVer.ofString('3.0.0-rc.4');
 	static var REPNAME = "lib";
 	static var SERVER = {
 		host : "lib.haxe.org",
