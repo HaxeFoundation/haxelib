73a5fae21fa40cfb34f1ce29a6cf2d83d449b71e
Zander Milroy <zander.milroy@gmail.com>
2013-07-20 14:40:36 -0400

    Added basic comparisons through satisfies()

--- src/tools/haxelib/SemVer.hx
+++ src/tools/haxelib/SemVer.hx
@@ -5,54 +5,98 @@ using Std;
 enum Preview {
 	ALPHA;
 	BETA;
-	RC;	
+	RC;
 }
 
 
 class SemVer {
+	public var precedence:String;
 	public var major:Int;
-	public var minor:Int;	
+	public var minor:Int;
 	public var patch:Int;
 	public var preview:Null<Preview>;
+	public var previewCmpVal:Int=999;
 	public var previewNum:Null<Int>;
-	public function new(major, minor, patch, ?preview, ?previewNum) {
+	public var previewNumCmpVal:Int=~(1<<31);
+	public function new(?precedence : String = "", major, minor, patch, ?preview, ?previewNum) {
+		this.precedence = precedence;
 		this.major = major;
 		this.minor = minor;
 		this.patch = patch;
 		this.preview = preview;
+		if ( this.preview != null ) {
+			previewCmpVal = Type.enumIndex( this.preview );
+		}
 		this.previewNum = previewNum;
+		if ( this.previewNum != null ) {
+			this.previewNumCmpVal = this.previewNum;
+		}
 	}
-	
+
 	public function toString():String {
-		var ret = '$major.$minor.$patch';
+		var ret = '$precedence$major.$minor.$patch';
 		if (preview != null) {
 			ret += '-' + preview.getName().toLowerCase();
-			if (previewNum != null) 
+			if (previewNum != null)
 				ret += '.' + previewNum;
 		}
 		return ret;
 	}
-	static var parse = ~/^([0-9]+)\.([0-9]+)\.([0-9]+)(-(alpha|beta|rc)(\.([0-9]+))?)?$/;
-	
-	static public function ofString(s:String):SemVer 
+
+	public function isSpecific():Bool {
+		return (precedence == "");
+	}
+
+	public function satisfies(v : SemVer):Bool {
+		if ( !this.isSpecific() ) { return false; }
+
+		return compareMain(v) || comparePre(v);
+	}
+
+	private function compareMain(other : SemVer) : Bool {
+		return cmp(major, other.precedence, other.major) || cmp(minor, other.precedence, other.minor) || cmp(patch, other.precedence, other.patch);
+	}
+
+	private function comparePre(other : SemVer) : Bool {
+		return cmp(previewCmpVal, other.precedence, other.previewCmpVal) || cmp(previewNumCmpVal, other.precedence, other.previewNumCmpVal);
+	}
+
+	private function cmp(a, op, b) : Bool {
+		var ret;
+		switch (op) {
+			case '', '=', '==': ret = (a == b);
+			case '!=': ret = (a != b);
+			case '>': ret = (a > b);
+			case '>=': ret = (a >= b);
+			case '<': ret = (a < b);
+			case '<=': ret = (a <= b);
+			default: throw 'Invalid operator: ' + op;
+		}
+		return ret;
+	}
+
+	static var parse = ~/^((?:<|>)?=?)([0-9]+)\.([0-9]+)\.([0-9]+)(-(alpha|beta|rc)(\.([0-9]+))?)?$/;
+
+	static public function ofString(s:String):SemVer
 		return
-			if (s!=null && parse.match(s.toLowerCase())) 
+			if (s!=null && parse.match(s.toLowerCase()))
 				new SemVer(
-					parse.matched(1).parseInt(),
+					parse.matched(1),
 					parse.matched(2).parseInt(),
 					parse.matched(3).parseInt(),
-					switch parse.matched(5) {
+					parse.matched(4).parseInt(),
+					switch parse.matched(6) {
 						case 'alpha': ALPHA;
 						case 'beta': BETA;
 						case 'rc': RC;
 						case v if (v == null): null;
 						case v: throw 'unrecognized preview tag $v';
 					},
-					switch parse.matched(7) {
+					switch parse.matched(8) {
 						case v if (v == null): null;
 						case v: v.parseInt();
 					}
 				)
-			else 
+			else
 				throw '$s is not a valid version string';//TODO: include some URL for reference
 }
--- test/tests/TestSemVer.hx
+++ test/tests/TestSemVer.hx
@@ -3,7 +3,7 @@ package tests;
 import tools.haxelib.SemVer;
 
 class TestSemVer extends haxe.unit.TestCase {
-	
+
 	public function testToString() {
 		assertEquals( "0.1.2", new SemVer(0,1,2).toString() );
 
@@ -11,19 +11,19 @@ class TestSemVer extends haxe.unit.TestCase {
 		assertEquals( "0.1.2-alpha", new SemVer(0,1,2,ALPHA).toString() );
 		assertEquals( "0.1.2-beta", new SemVer(0,1,2,BETA).toString() );
 		assertEquals( "0.1.2-rc", new SemVer(0,1,2,RC).toString() );
-		
+
 		// Release Tag Versions
 		assertEquals( "0.1.2-alpha.0", new SemVer(0,1,2,ALPHA,0).toString() );
 		assertEquals( "0.1.2-beta.0", new SemVer(0,1,2,BETA,0).toString() );
 		assertEquals( "0.1.2-rc.0", new SemVer(0,1,2,RC,0).toString() );
-		
+
 		// Weird input
 		assertEquals( "0.1.2", new SemVer(0,1,2,null,0).toString() );
-		
+
 		// Multiple characters
 		assertEquals( "100.200.300-rc.400", new SemVer(0100,0200,0300,RC,0400).toString() );
 	}
-	
+
 	public function testOfString() {
 		// Normal
 		assertEquals( "0.1.2", SemVer.ofString("0.1.2").toString() );
@@ -35,8 +35,43 @@ class TestSemVer extends haxe.unit.TestCase {
 		assertEquals( "0.1.2-beta", SemVer.ofString("0.1.2-beta").toString() );
 		assertEquals( "0.1.2-rc", SemVer.ofString("0.1.2-rc").toString() );
 		assertEquals( "0.1.2-rc.1", SemVer.ofString("0.1.2-rc.01").toString() );
+		assertEquals( "<0.1.2-rc.1", SemVer.ofString("<0.1.2-rc.01").toString() );
+		assertEquals( ">0.1.2-rc.1", SemVer.ofString(">0.1.2-rc.01").toString() );
+		assertEquals( "<=0.1.2-rc.1", SemVer.ofString("<=0.1.2-rc.01").toString() );
 	}
-	
+
+	public function testSpecificity() {
+		assertTrue(SemVer.ofString("0.1.2-rc.01").isSpecific());
+		assertFalse(SemVer.ofString(">0.1.2-rc.01").isSpecific());
+	}
+
+	public function testComparisons() {
+		assertTrue( SemVer.ofString("0.1.2-rc.01").satisfies( SemVer.ofString("<=0.1.2-rc.01") ) );
+		assertFalse( SemVer.ofString("0.1.2-rc.01").satisfies( SemVer.ofString("<0.1.2-rc.01") ) );
+		assertFalse( SemVer.ofString("0.1.2-rc.01").satisfies( SemVer.ofString(">0.1.2-rc.01") ) );
+
+		assertTrue( SemVer.ofString("1.0.0").satisfies( SemVer.ofString(">0.1.2") ) );
+		assertTrue( SemVer.ofString("1.0.0").satisfies( SemVer.ofString("<1.0.4") ) );
+		assertTrue( SemVer.ofString("1.0.0").satisfies( SemVer.ofString(">=0.9.3") ) );
+		assertTrue( SemVer.ofString("1.0.0").satisfies( SemVer.ofString("<=1.0.9") ) );
+
+		assertTrue( SemVer.ofString("1.0.0-rc").satisfies( SemVer.ofString(">1.0.0-alpha") ) );
+		assertTrue( SemVer.ofString("1.0.0-rc").satisfies( SemVer.ofString(">1.0.0-beta") ) );
+		assertFalse( SemVer.ofString("1.0.0-rc").satisfies( SemVer.ofString(">1.0.0-rc") ) );
+
+		assertFalse( SemVer.ofString("1.0.0-beta").satisfies( SemVer.ofString(">1.0.0-rc") ) );
+		assertTrue( SemVer.ofString("1.0.0-beta").satisfies( SemVer.ofString(">1.0.0-alpha") ) );
+
+		assertTrue( SemVer.ofString("0.1.3").satisfies( SemVer.ofString(">0.1.3-alpha") ) );
+		assertFalse( SemVer.ofString("0.1.2-alpha.9").satisfies( SemVer.ofString("<=0.1.2-alpha.8") ) );
+
+		assertTrue( SemVer.ofString("0.1.0-beta").satisfies( SemVer.ofString(">0.1.0-alpha") ) );
+		assertFalse( SemVer.ofString("0.1.2-beta").satisfies( SemVer.ofString(">0.1.2-rc.1") ) );
+
+		assertFalse( SemVer.ofString("1.0.0-alpha").satisfies( SemVer.ofString("<=1.0.0-alpha") ) );
+		assertFalse( SemVer.ofString("1.0.0").satisfies( SemVer.ofString("<=1.0.0-alpha") ) );
+	}
+
 	public function testOfStringInvalid() {
 		assertEquals( "invalid", parseInvalid(null) );
 		assertEquals( "invalid", parseInvalid("") );
@@ -58,5 +93,5 @@ class TestSemVer extends haxe.unit.TestCase {
 			"invalid";
 		}
 	}
-	
+
 }
