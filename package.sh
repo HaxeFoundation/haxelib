ed5ab684ca4dcb96ce7af264c296cc0049a1f7ac
Andy Li <andy@onthewings.net>
2014-03-15 00:41:33 +0800

    Package haxelib using the same structure of the repo.

--- package.sh
+++ package.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-mkdir -p package/tools/haxelib/
-cp src/tools/haxelib/Data.hx package/tools/haxelib/
-cp src/tools/haxelib/Main.hx package/tools/haxelib/
-cp src/tools/haxelib/Rebuild.hx package/tools/haxelib/
-cp src/tools/haxelib/SemVer.hx package/tools/haxelib/
-cp src/tools/haxelib/SiteApi.hx package/tools/haxelib/
-cp src/tools/haxelib/ConvertXml.hx package/tools/haxelib/
+mkdir -p package/src/tools/haxelib
+cp src/tools/haxelib/Data.hx package/src/tools/haxelib/
+cp src/tools/haxelib/Main.hx package/src/tools/haxelib/
+cp src/tools/haxelib/Rebuild.hx package/src/tools/haxelib/
+cp src/tools/haxelib/SemVer.hx package/src/tools/haxelib/
+cp src/tools/haxelib/SiteApi.hx package/src/tools/haxelib/
+cp src/tools/haxelib/ConvertXml.hx package/src/tools/haxelib/
 cp haxelib.json package/haxelib.json
 
 rm -Rf package/package.zip
 cd package
-zip -r package.zip haxelib.json tools
+zip -r package.zip *
 cd ..
