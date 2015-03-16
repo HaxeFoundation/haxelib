9ea04a098ab4d5d614709f3f985661bf8792dda7
Jason O'Neil <jason.oneil@gmail.com>
2015-03-16 19:31:52 +0800

    Use `-cwd` in shell scripts calling haxelib.
    See #109.
    
    Still need to update compiler Makefile to use this in their script.
    
    Then we can close this issue.

--- src/tools/haxelib/Main.hx
+++ src/tools/haxelib/Main.hx
@@ -1039,18 +1039,22 @@ class Main {
 					var p = new Process('haxelib', ['path', 'haxelib_client']);
 					if (p.exitCode() == 0) {
 						var args = [];
+						var classPath = "";
 						for (arg in p.stdout.readAll().toString().split('\n')) {
 							arg = arg.trim();
 							if (arg.charAt(0) == '-')
 								args.push(arg);
 							else if (arg.length > 0)
-								args.push('-cp "$arg"');
+								classPath = arg;
 						};
 
 						var file = haxepath+'haxelib';
 						try File.saveContent(
 							file,
-							'#!/bin/sh\nexec haxe '+args.join(' ')+' --run tools.haxelib.Main "$@"'
+							'#!/bin/sh'
+							+'\n'+'OLDCWD=`pwd`'
+							+'\n'+'cd $classPath'
+							+'\n'+'exec haxe '+args.join(' ')+" --run tools.haxelib.Main -cwd $OLDCWD $@"
 						)
 						catch (e:Dynamic)
 							throw 'Error writing file $file. Please ensure you have write permissions. \n  ' + Std.string(e);
