74a9d24e2c77d3209fac269fd1b9ad791563c546
Alex Koz <i@fizzer.ru>
2015-04-12 14:13:14 +0300

    fixed test

--- test-issue-202.sh
+++ test-issue-202.sh
@@ -2,7 +2,10 @@
 echo "haxelib remove symlinks (issue #202) with nme-dev 1.3.2"
 haxelib install nme-dev 1.3.2 --always
 
-if [ "$(haxelib remove nme-dev)" == "Library nme-dev removed" ]; then
+HAXELIB_REMOVE_SYM_RESULT="$(haxelib remove nme-dev)"
+echo $HAXELIB_REMOVE_SYM_RESULT
+
+if [ "$HAXELIB_REMOVE_SYM_RESULT" == "Library nme-dev removed" ]; then
  echo "     test passed"
  exit 0
 else
