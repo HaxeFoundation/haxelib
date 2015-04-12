c9372611a3da6edd31934982be791bd9e0073cf0
Alex Koz <i@fizzer.ru>
2015-04-12 14:46:51 +0300

    fixed `eq` for Travis Ci (http://docs.travis-ci.com/user/customizing-the-build/#Implementing-Complex-Build-Steps)

--- test-issue-202.sh
+++ test-issue-202.sh
@@ -5,7 +5,7 @@ haxelib install nme-dev 1.3.2 --always
 HAXELIB_REMOVE_SYM_RESULT="$(haxelib remove nme-dev)"
 echo $HAXELIB_REMOVE_SYM_RESULT
 
-if [ "$HAXELIB_REMOVE_SYM_RESULT" == "Library nme-dev removed" ]; then
+if [ "$HAXELIB_REMOVE_SYM_RESULT" = "Library nme-dev removed" ]; then
  echo "     test passed"
  exit 0
 else
