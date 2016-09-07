HaxeFoundation/haxelib

commit ce703c7d93c9297b2fbc8804f0c9fb00cc6f8c9a

Website: only update a project shown version if it's a higher semver (#346)

src/haxelib/server/Repo.hx
@@ -315,7 +315,11 @@ class Repo implements SiteApi {
 				v.documentation = doc;
 				v.insert();
 
-				p.versionObj = v;
+				// p.versionObj is the one shown on the website
+				if (p.versionObj != null && p.versionObj.toSemver() < v.toSemver()) {
+					p.versionObj = v;
+				}
+
 				p.update();
 				return "Version " + v.toSemver() + " (id#" + v.id + ") added";
 			}

commit e79311c56fe600a9a8f753fbe5883e29a19bd4d6

fixed haxe 3.1.3 issue caused by #346

src/haxelib/server/Repo.hx
@@ -316,7 +316,7 @@ class Repo implements SiteApi {
 				v.insert();
 
 				// p.versionObj is the one shown on the website
-				if (p.versionObj != null && p.versionObj.toSemver() < v.toSemver()) {
+				if (p.versionObj == null || p.versionObj.toSemver() < v.toSemver()) {
 					p.versionObj = v;
 				}
 