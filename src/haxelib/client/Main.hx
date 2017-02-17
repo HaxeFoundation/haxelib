801b0b9d9498fd747446dcd39e6707527bff4a23
Gama11 <jensfischer95@gmail.com>
2017-02-17 21:33:53 +0100

    Typo fix for "haxelib version" help

--- src/haxelib/client/Main.hx
+++ src/haxelib/client/Main.hx
@@ -174,7 +174,7 @@ class Main {
 		addCommand("user", user, "list information on a given user", Information);
 		addCommand("config", config, "print the repository path", Information, false);
 		addCommand("path", path, "give paths to libraries", Information, false);
-		addCommand("version", version, "print the currently using haxelib version", Information, false);
+		addCommand("version", version, "print the currently used haxelib version", Information, false);
 		addCommand("help", usage, "display this list of options", Information, false);
 
 		addCommand("submit", submit, "submit or update a library package", Development);
