b50e8f3d6e2eb37951c297c7a2f9806f63ddbdd4
Emin Fedar <eminfedar@gmail.com>
2019-06-04 12:48:39 +0300

    fix: #451 (#455)
    * fix: #451
    
    * Improvements
    
    * Improvements

--- src/haxelib/client/Main.hx
+++ src/haxelib/client/Main.hx
@@ -244,7 +244,13 @@ class Main {
 	}
 
 	function version() {
-		print(VERSION_LONG);
+		var params = paramOpt();
+		if ( params == null )
+			print(VERSION_LONG);
+		else {
+			Sys.stderr().writeString('no parameters expected, got: ${params}\n');
+			Sys.exit(1);
+		}
 	}
 
 	function usage() {
