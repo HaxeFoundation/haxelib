96b3a019bed914275e67e70416c8311e52905f30
kaikoga <info@mail.kaikoga.net>
2019-09-17 05:15:15 +0900

    fix #458 while trying to do what #447 meant to

--- src/haxelib/client/Main.hx
+++ src/haxelib/client/Main.hx
@@ -1566,6 +1566,9 @@ class Main {
 
 		var libPath = proj + "/" + vcs.directory;
 
+		var willClone = false;
+		var willUpdate = false;
+
 		if ( FileSystem.exists(proj + "/" + Data.safe(vcs.directory)) ) {
 			print("You already have "+libName+" version "+vcs.directory+" installed.");
 
@@ -1576,16 +1579,21 @@ class Main {
 				&& ask("Overwrite branch: " + (currentBranch == null?"<unspecified>":"\"" + currentBranch + "\"") + " with \"" + branch + "\""))
 			{
 				deleteRec(libPath);
-				this.alreadyUpdatedVcsDependencies.set(libName, branch);
+				willClone = true;
 			}
 			else if (!wasUpdated)
 			{
-				print("Updating " + libName+" version " + vcs.directory + " ...");
-				updateByName(rep, libName);
+				willUpdate = true;
 			}
 		} else {
-			print("Installing " +libName + " from " +url + ( branch != null ? " branch: " + branch : "" ));
+			willClone = true;
+		}
 
+		if (willUpdate) {
+			print("Updating " + libName+" version " + vcs.directory + " ...");
+			updateByName(rep, libName);
+		} else if (willClone) {
+			print("Installing " +libName + " from " +url + ( branch != null ? " branch: " + branch : "" ));
 			try {
 				vcs.clone(libPath, url, branch, version);
 			} catch(error:VcsError) {
