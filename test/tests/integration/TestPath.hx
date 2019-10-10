0a736d31e764ce765886b21b7a61837a043cb5bf
Aleksandr Kuzmenko <alex@stablex.ru>
2019-10-10 17:26:41 +0300

    only test for non-system haxelib binary

--- test/tests/integration/TestPath.hx
+++ test/tests/integration/TestPath.hx
@@ -4,10 +4,12 @@ import haxe.io.Path;
 using IntegrationTests;
 
 class TestPath extends IntegrationTests {
+#if !system_haxelib
 	function testMain():Void {
 		var r = haxelib(["dev", "BadHaxelibJson", Path.join([IntegrationTests.projectRoot, "test/libraries/libBadHaxelibJson"])]).result();
 		assertSuccess(r);
 		var r = haxelib(["path", "BadHaxelibJson"]).result();
 		assertFail(r);
 	}
+#end
 }
