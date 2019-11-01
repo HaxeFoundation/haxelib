a89fd7a5c7a1077eb2edf467dd724ea0a0b77e8b
Aleksandr Kuzmenko <alex@stablex.ru>
2019-11-01 18:15:52 +0300

    like this?

--- src/haxelib/Data.hx
+++ src/haxelib/Data.hx
@@ -63,7 +63,9 @@ abstract DependencyVersion(String) to String from SemVer {
 
 	@:to function toValidatable():Validatable
 		return
-			if (this == null || this == '' || this == 'git' || this.startsWith('git:'))
+			if (this == '' || this == 'git')
+				{ validate: function () return None }
+			else if(this != null && this.startsWith('git:'))
 				{ validate: function () return None }
 			else
 				@:privateAccess new SemVer(this);
