c663137a1790637f4f498c53a1477f89a61d8bbe
Aleksandr Kuzmenko <alex@stablex.ru>
2019-11-01 19:17:18 +0300

    cleanup [skip ci]

--- test/tests/integration/TestPath.hx
+++ test/tests/integration/TestPath.hx
@@ -13,16 +13,5 @@ class TestPath extends IntegrationTests {
 		var r = haxelib(["path", "BadHaxelibJson"]).result();
 		assertFail(r);
 	}
-
-	// function testGitDep():Void {
-	// 	var r = haxelib(["dev", "UseGitDep", Path.join([IntegrationTests.projectRoot, "test/libraries/UseGitDep"])]).result();
-	// 	assertSuccess(r);
-	// 	var r = haxelib(["path", "UseGitDep"]).result();
-	// 	assertSuccess(r);
-	// 	var paths = r.out.split('\n')
-	// 		.filter(function(line) return !line.startsWith('-'))
-	// 		.map(function(p) return new Path(p.trim()));
-	// 	assertTrue(paths.exists(function(p) return p.dir.contains('signal') && p.dir.endsWith('git')));
-	// }
 #end
 }
