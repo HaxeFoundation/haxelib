1f00300f7a9c804641dc53408de4a6400703d5ef
tobil4sk <tobil4sk@outlook.com>
2021-04-18 21:43:40 +0100

    [tests] Fix integration test

--- test/tests/integration/TestSetup.hx
+++ test/tests/integration/TestSetup.hx
@@ -1,12 +1,11 @@
 package tests.integration;
 
-import haxelib.client.Main;
+import haxelib.client.RepoManager;
 
 class TestSetup extends IntegrationTests {
 	function testCleanEnv():Void {
 		// remove .haxelib to simulate an enviroment that haven't `haxelib setup` yet
-        final config = Main.getConfigFile();
-        sys.FileSystem.deleteFile(config);
+		RepoManager.unsetGlobalPath();
 
 		final installResult = haxelib(["setup", originalRepo]).result();
 		assertEquals(0, installResult.code);
