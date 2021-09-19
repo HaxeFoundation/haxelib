f00bfd5eb7a83c8297eaffb9dd87c998a3a315f7
Andy Li <andy@onthewings.net>
2021-09-19 15:51:21 +0800

    bump DNS records TTL to 24 hrs

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -6,7 +6,7 @@ resource "aws_route53_record" "lib" {
   zone_id = local.haxe_org_zoneid
   name    = "lib.haxe.org"
   type    = "CNAME"
-  ttl     = "30"
+  ttl     = "86400"
   records = ["k8s.haxe.org"]
 }
 
@@ -14,7 +14,7 @@ resource "aws_route53_record" "development-lib" {
   zone_id = local.haxe_org_zoneid
   name    = "development-lib.haxe.org"
   type    = "CNAME"
-  ttl     = "30"
+  ttl     = "86400"
   records = ["k8s.haxe.org"]
 }
 
