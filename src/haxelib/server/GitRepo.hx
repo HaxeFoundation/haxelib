68d45caa65b1e471901676374c21a9400ada1be0
Andy Li <andy@onthewings.net>
2021-09-26 02:07:34 +0800

    octokit setup

--- src/haxelib/server/GitRepo.hx
+++ src/haxelib/server/GitRepo.hx
@@ -1,15 +1,32 @@
 package haxelib.server;
 
-import octokit.Octokit;
 import octokit.AuthApp;
 
 class GitRepo {
-    static function main():Void {
-        var octokit = new Octokit({
-            authStrategy: AuthApp.createAppAuth,
-            auth: {
+    // Auth as an "installation"
+    // https://github.com/organizations/haxelib/settings/installations/19601456
+    static public final githubApp = {
+        appId: 139127,
+        clientId: "Iv1.89d7675c50ba41de",
+        privateKey: Sys.getEnv("GITHUB_APP_PRIVATE_KEY"),
+        installationId: 19601456,
+    }
 
-            },
+    static function main():Void {
+        var octokit = new octokit.core.Octokit({
+            authStrategy: AuthApp.createAppAuth,
+            auth: githubApp,
         });
+        octokit.request.call({
+            method: "POST",
+            url: "/orgs/{org}/repos",
+            org: "haxelib",
+            name: "test_createdWithOctokit",
+            "private": true,
+        })
+            .then(r -> {
+                trace(r);
+            })
+            .catchError(err -> trace(err));
     }
 }
