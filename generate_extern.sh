44e47f652a14a9334f0b9f5e92b4c5c9ebed3404
Andy Li <andy@onthewings.net>
2021-09-28 09:53:05 +0000

    update dts2hx

--- generate_extern.sh
+++ generate_extern.sh
@@ -16,8 +16,3 @@ npx dts2hx \
     --useSystemHaxe \
     --output \
     "$OUTPUT_DIR"
-
-# https://github.com/haxiomic/dts2hx/issues/98
-# find "$OUTPUT_DIR" -type f -exec sed -i 's/js.lib.AsyncIterator/js.lib.AsyncIterator_/g' {} +
-# mv "$OUTPUT_DIR"/js/lib/AsyncIterator.hx "$OUTPUT_DIR"/js/lib/AsyncIterator_.hx
-# sed -i 's/AsyncIterator/AsyncIterator_/g' "$OUTPUT_DIR"/js/lib/AsyncIterator_.hx
