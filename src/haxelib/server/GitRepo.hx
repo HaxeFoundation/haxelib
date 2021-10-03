8404330a45d21ac97085e0bc0c7512fcb3a86c7e
Andy Li <andy@onthewings.net>
2021-10-03 17:06:30 +0800

    improve how haxelib archives are imported to a git repo

--- src/haxelib/server/GitRepo.hx
+++ src/haxelib/server/GitRepo.hx
@@ -1,8 +1,11 @@
 package haxelib.server;
 
+import sys.io.File;
+import haxelib.Data;
 import haxelib.server.Paths;
 import haxelib.server.Paths.*;
 import haxe.io.Path;
+import haxe.ds.*;
 import sys.FileSystem;
 import js.lib.Promise;
 import octokit.AuthApp;
@@ -83,6 +86,10 @@ class GitRepo {
         FileSystem.createDirectory(gitRepoDir);
         var gitRepo = git({
             baseDir: gitRepoDir,
+            config: [
+                'user.name=haxelib',
+                'user.email=contact@haxe.org',
+            ]
         }).init(false);
 
         var haxelibRepo = new Repo();
@@ -93,47 +100,95 @@ class GitRepo {
             }
         }
 
-        // from smaller to larger
-        infos.versions.sort(function (a, b) return SemVer.compare(a.name, b.name));
-        //trace(infos.versions.map(v -> v.name));
+        // from earlier to later
+        var versionsDateSorted:ReadOnlyArray<VersionInfos> = {
+            var vs = infos.versions.copy();
+            vs.sort(function (a, b) return Reflect.compare(a.date, b.date));
+            vs;
+        }
 
         Sys.setCwd(TMP_DIR);
+        var client = new haxelib.client.Main();
+        client.settings = {
+            debug: false,
+            quiet: true,
+            flat: false,
+            always: true,
+            never: false,
+            global: false,
+            system: false,
+            skipDependencies: false,
+        };
+        if (Path.normalize(client.getRepository()) != Path.normalize(localHaxelibDir)) {
+            throw "haxelib is not setup properly";
+        }
 
-        function importVersions(versions:Array<SemVer>) {
+        function importVersions(versions:ReadOnlyArray<SemVer>) {
             if (versions.length <= 0)
                 return Promise.resolve();
 
-            var version = versions.shift();
-            var localVersionDir:AbsPath = Path.join([localHaxelibDir, Data.safe(haxelib), Data.safe(version)]);
-            return FsExtra.readdir(localVersionDir)
-                .then(files -> Promise.all([
-                    for (file in files)
-                        FsExtra.copy(
-                            Path.join([localVersionDir, file]),
-                            Path.join([gitRepoDir, file]),
-                            {
-                                recursive: true,
-                            }
+            final version = versions[0];
+            final importedVersions:Promise<Array<SemVer>> = gitRepo.tags().then(r -> {
+                var versions = r.all.map(SemVer.ofString);
+                // from smaller to larger
+                versions.sort(function (a, b) return SemVer.compare(a, b));
+                versions;
+            });
+            final parentVersion = importedVersions.then(verions -> {
+                var parent = null;
+                for (v in verions) {
+                    if (v >= version)
+                        break;
+                    parent = v;
+                }
+                parent;
+            });
+            final localVersionDir:AbsPath = Path.join([localHaxelibDir, Data.safe(haxelib), Data.safe(version)]);
+            if (!FileSystem.exists(localVersionDir)) {
+                client.doInstall(client.getRepository(), haxelib, version, true);
+            }
+            return parentVersion
+                .then(parentVersion -> parentVersion == null ? null : gitRepo.checkout(parentVersion))
+                .then(_ ->
+                    // replace the files in git workspace with the haxelib archive contents
+                    FsExtra.readdir(gitRepoDir)
+                        .then(files -> Promise.all([
+                            for (file in files)
+                            if (file != ".git")
+                            FsExtra.remove(Path.join([gitRepoDir, file]))
+                        ]))
+                        .then(_ -> trace("cleared git workspace"))
+                        .then(_ ->
+                            FsExtra.readdir(localVersionDir)
+                                .then(files -> Promise.all([
+                                    for (file in files)
+                                        FsExtra.copy(
+                                            Path.join([localVersionDir, file]),
+                                            Path.join([gitRepoDir, file]),
+                                            {
+                                                recursive: true,
+                                            }
+                                        )
+                                ]))
                         )
-                ]))
+                        .then(_ -> trace("copied files"))
+                )
                 .then(_ -> gitRepo.add("--all"))
-                .then(_ -> gitRepo.commit('import $haxelib $version', {
-                    "--author": "Testing <testing@example.com>",
-                }))
-                .then(_ -> gitRepo.status().then(s -> {
-                    trace(s);
-                    null;
-                }));
+                .then(_ -> gitRepo.commit('import $haxelib $version'))
+                .then(_ -> gitRepo.addTag(version))
+                .then(_ -> trace('imported $haxelib $version'))
+                .then(_ -> importVersions(versions.slice(1)));
         }
 
-        importVersions(infos.versions.map(v -> v.name));
+        importVersions(versionsDateSorted.map(v -> v.name));
     }
 
     static function main():Void {
         FileSystem.createDirectory(localGitDir);
         FileSystem.createDirectory(localHaxelibDir);
 		SiteDb.init();
-        importToRemote("lime");
+        // installAllVersions("jQueryExtern");
+        importToRemote("jQueryExtern");
 		SiteDb.cleanup();
     }
 }
