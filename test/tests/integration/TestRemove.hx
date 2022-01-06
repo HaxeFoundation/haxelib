488b96a5b33136467631411362f887dcb55e5b0c
tobil4sk <tobil4sk@outlook.com>
2022-01-06 21:24:16 +0000

    [tests] Add more testing for remove command
    For removing specific versions

--- test/tests/integration/TestRemove.hx
+++ test/tests/integration/TestRemove.hx
@@ -2,47 +2,68 @@ package tests.integration;
 
 class TestRemove extends IntegrationTests {
 
-	override function setup() {
-		super.setup();
-		final r = haxelib(["register", bar.user, bar.email, bar.fullname, bar.pw, bar.pw]).result();
-		assertSuccess(r);
-
-		final r = haxelib([
-			"submit",
-			Path.join([IntegrationTests.projectRoot, "test/libraries/libBar.zip"]),
-			bar.pw
-		]).result();
-		assertSuccess(r);
-
-		final r = haxelib(["install", "Bar"]).result();
-		assertSuccess(r);
-	}
 
 
 	function testNormal():Void {
-		{
-			final r = haxelib(["remove", "Bar"]).result();
-			assertSuccess(r);
-		}
+		final r = haxelib(["install", "libraries/libBar.zip"]).result();
+		assertSuccess(r);
 
-		{
-			final r = haxelib(["list", "Bar"]).result();
-			assertSuccess(r);
-			assertTrue(r.out.indexOf("Bar") < 0);
-		}
+		final r = haxelib(["remove", "Bar"]).result();
+		assertSuccess(r);
+
+		final r = haxelib(["list", "Bar"]).result();
+		assertSuccess(r);
+		assertTrue(r.out.indexOf("Bar") < 0);
 	}
 
 	// for issue #529
 	function testDifferentCapitalization() {
-		{
-			final r = haxelib(["remove", "bar"]).result();
-			assertSuccess(r);
-		}
+		final r = haxelib(["install", "libraries/libBar.zip"]).result();
+		assertSuccess(r);
 
-		{
-			final r = haxelib(["list", "bar"]).result();
-			assertSuccess(r);
-			assertTrue(r.out.indexOf("Bar") < 0);
-		}
+		final r = haxelib(["remove", "bar"]).result();
+		assertSuccess(r);
+
+		final r = haxelib(["list", "bar"]).result();
+		assertSuccess(r);
+		assertTrue(r.out.indexOf("Bar") < 0);
+	}
+
+	function testRemoveSpecificVersion() {
+		final r = haxelib(["install", "libraries/libBar.zip"]).result();
+		assertSuccess(r);
+
+		final r = haxelib(["install", "libraries/libBar2.zip"]).result();
+		assertSuccess(r);
+
+		final r = haxelib(["list", "Bar"]).result();
+		assertSuccess(r);
+		assertTrue(r.out.indexOf("Bar") >= 0);
+		assertTrue(r.out.indexOf("1.0.0") >= 0);
+		assertTrue(r.out.indexOf("[2.0.0]") >= 0);
+
+		// non current version can be removed
+		final r = haxelib(["remove", "Bar", "1.0.0"]).result();
+		assertSuccess(r);
+
+		final r = haxelib(["list", "Bar"]).result();
+		assertSuccess(r);
+		assertTrue(r.out.indexOf("Bar") >= 0);
+		assertTrue(r.out.indexOf("1.0.0") < 0);
+		assertTrue(r.out.indexOf("[2.0.0]") >= 0);
+
+		// current version cannot be removed
+		final r = haxelib(["remove", "Bar", "2.0.0"]).result();
+		assertFail(r);
+		assertEquals(
+			"Error: Cannot remove current version of library Bar",
+			r.err.trim()
+		);
+
+		final r = haxelib(["list", "Bar"]).result();
+		assertSuccess(r);
+		assertTrue(r.out.indexOf("Bar") >= 0);
+		assertTrue(r.out.indexOf("1.0.0") < 0);
+		assertTrue(r.out.indexOf("[2.0.0]") >= 0);
 	}
 }
