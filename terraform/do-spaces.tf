9cff6053e4413f1773a27bf7ab3e38473eaefabe
Andy Li <andy@onthewings.net>
2022-01-24 17:03:32 +0800

    let the DO cluster uses spaces

--- terraform/do-spaces.tf
+++ terraform/do-spaces.tf
@@ -8,7 +8,51 @@ resource "random_string" "do-haxelib-bucket-suffix" {
 resource "digitalocean_spaces_bucket" "haxelib" {
   name   = "haxelib-${random_string.do-haxelib-bucket-suffix.result}"
   region = "fra1"
-  acl    = "private"
+  acl    = "public-read"
+}
+
+resource "digitalocean_cdn" "haxelib" {
+  origin = digitalocean_spaces_bucket.haxelib.bucket_domain_name
+}
+
+provider "aws" {
+  alias = "do-spaces"
+
+  access_key = data.kubernetes_secret.haxelib-server-do-spaces.data.SPACES_ACCESS_KEY_ID
+  secret_key = data.kubernetes_secret.haxelib-server-do-spaces.data.SPACES_SECRET_ACCESS_KEY
+  endpoints {
+    s3 = "${digitalocean_spaces_bucket.haxelib.region}.digitaloceanspaces.com"
+  }
+
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+}
+
+resource "aws_s3_bucket_policy" "do-public-read" {
+  provider = aws.do-spaces
+  bucket   = digitalocean_spaces_bucket.haxelib.name
+  policy   = data.aws_iam_policy_document.do-public-read.json
+}
+
+data "aws_iam_policy_document" "do-public-read" {
+  provider = aws.do-spaces
+  statement {
+    sid = "PublicReadGetObject"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${digitalocean_spaces_bucket.haxelib.name}/*",
+    ]
+  }
 }
 
 data "kubernetes_secret" "haxelib-server-do-spaces" {
@@ -39,7 +83,7 @@ resource "kubernetes_secret" "rclone-haxelib-s3-to-spaces-config" {
       env_auth = false
       access_key_id = ${data.kubernetes_secret.haxelib-server-do-spaces.data.SPACES_ACCESS_KEY_ID}
       secret_access_key = ${data.kubernetes_secret.haxelib-server-do-spaces.data.SPACES_SECRET_ACCESS_KEY}
-      endpoint = fra1.digitaloceanspaces.com
+      endpoint = ${digitalocean_spaces_bucket.haxelib.region}.digitaloceanspaces.com
       acl = private
     EOT
   }
