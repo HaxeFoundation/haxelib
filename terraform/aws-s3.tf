3e13c4bc806a36a2075baef213c613550e82ee47
Andy Li <andy@onthewings.net>
2022-06-08 09:44:56 +0800

    upgrade aws_s3_bucket

--- terraform/aws-s3.tf
+++ terraform/aws-s3.tf
@@ -1,25 +1,36 @@
 resource "aws_s3_bucket" "lib-haxe-org" {
   bucket = "lib.haxe.org"
+}
 
-  grant {
-    id = data.aws_canonical_user_id.current.id
-    permissions = [
-      "READ",
-      "READ_ACP",
-      "WRITE",
-      "WRITE_ACP"
-    ]
-    type = "CanonicalUser"
-  }
-  grant {
-    id = "c4c1ede66af53448b93c283ce9448c4ba468c9432aa01d700d3878632f77d2d0"
-    permissions = [
-      "FULL_CONTROL",
-    ]
-    type = "CanonicalUser"
-  }
+resource "aws_s3_bucket_acl" "lib-haxe-org" {
+  bucket = aws_s3_bucket.lib-haxe-org.bucket
+  access_control_policy {
+    grant {
+      grantee {
+        id   = data.aws_canonical_user_id.current.id
+        type = "CanonicalUser"
+      }
+      permission = "FULL_CONTROL"
+    }
 
-  website {
-    index_document = "index.html"
+    grant {
+      grantee {
+        id   = "c4c1ede66af53448b93c283ce9448c4ba468c9432aa01d700d3878632f77d2d0"
+        type = "CanonicalUser"
+      }
+      permission = "FULL_CONTROL"
+    }
+
+    owner {
+      id = data.aws_canonical_user_id.current.id
+    }
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "lib-haxe-org" {
+  bucket = aws_s3_bucket.lib-haxe-org.bucket
+
+  index_document {
+    suffix = "index.html"
   }
 }
