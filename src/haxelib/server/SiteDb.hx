88ffe1e9851b773933c40c1cfb381013a1397a2e
tobil4sk <tobil4sk@outlook.com>
2022-09-21 17:59:58 +0100

    Implement database updating system
    This allows password to be automatically rehashed if the database is old

--- src/haxelib/server/SiteDb.hx
+++ src/haxelib/server/SiteDb.hx
@@ -118,6 +118,11 @@ class Developer extends Object {
 
 }
 
+class Meta extends Object {
+	var id:SString<1>;
+	public var dbVersion:SUInt;
+}
+
 class SiteDb {
 	static var db : Connection;
 	//TODO: this whole configuration business is rather messy to say the least
@@ -146,7 +151,8 @@ class SiteDb {
 			Project.manager,
 			Tag.manager,
 			Version.manager,
-			Developer.manager
+			Developer.manager,
+			Meta.manager
 		];
 		for (m in managers)
 			if (!TableCreate.exists(m))
