f8b87e91421d975c9a34431af3f90b1f4678b7ca
tobil4sk <tobil4sk@outlook.com>
2023-04-06 11:05:56 +0100

    Add tests for dependency checks in submit

--- test/tests/integration/TestSubmit.hx
+++ test/tests/integration/TestSubmit.hx
@@ -46,6 +46,40 @@ class TestSubmit extends IntegrationTests {
 		}
 	}
 
+	function testLibraryWithMissingDep() {
+		final r = haxelib(["register", foo.user, foo.email, foo.fullname, foo.pw, foo.pw]).result();
+		assertSuccess(r);
+
+		final r = haxelib(["submit", Path.join([IntegrationTests.projectRoot, "test/libraries/libMissingDep.zip"]), foo.pw]).result();
+		assertFail(r);
+		assertEquals("Error: Library MissingDep does not exist", r.err.trim());
+
+		final r = haxelib(["search", "Foo"]).result();
+		// did not get submitted
+		assertFalse(r.out.indexOf("Foo") >= 0);
+	}
+
+	function testLibraryWithMissingDepVersion() {
+		final r = haxelib(["register", bar.user, bar.email, bar.fullname, bar.pw, bar.pw]).result();
+		assertSuccess(r);
+
+		final r = haxelib(["submit", Path.join([IntegrationTests.projectRoot, "test/libraries/libBar.zip"]), bar.pw]).result();
+		assertSuccess(r);
+
+		final r = haxelib(["register", foo.user, foo.email, foo.fullname, foo.pw, foo.pw]).result();
+		assertSuccess(r);
+
+		final r = haxelib(["submit", Path.join([IntegrationTests.projectRoot, "test/libraries/libMissingDepVersion.zip"]), foo.pw]).result();
+		assertFail(r);
+		assertEquals("Error: Library Bar does not have version 2.0.0", r.err.trim());
+
+		trace("hello");
+
+		final r = haxelib(["search", "Foo"]).result();
+		// did not get submitted
+		assertFalse(r.out.indexOf("Foo") >= 0);
+	}
+
 	function testLibraryWithGitDep() {
 		// git deps should not be allowed in published versions
 		// https://github.com/HaxeFoundation/haxelib/pull/344#issuecomment-244006799
