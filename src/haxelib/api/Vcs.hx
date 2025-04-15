649cd60b966b1b1e69cc8df2ae10ea2512298f76
Tobiasz Laskowski <tobil4sk@outlook.com>
2025-04-15 17:41:11 +0100

    Revert "Use threads only for git processes on windows"
    This reverts commit b8c24a783000c609ae3b45c27aab423d9e0d7193.
    
    It was reverted due to: https://github.com/HaxeFoundation/neko/issues/281,
    however this issue has since been patched in neko. It is important to
    read both streams at the same time to avoid either of them filling up
    and causing the process to block.

--- src/haxelib/api/Vcs.hx
+++ src/haxelib/api/Vcs.hx
@@ -194,39 +194,28 @@ abstract class Vcs implements IVcs {
 		// just in case process hangs waiting for stdin
 		p.stdin.close();
 
-		final ret = if (Sys.systemName() == "Windows") {
-			final streamsLock = new sys.thread.Lock();
-			function readFrom(stream:haxe.io.Input, to:{value:String}) {
-				to.value = stream.readAll().toString();
-				streamsLock.release();
-			}
+		final streamsLock = new sys.thread.Lock();
+		function readFrom(stream:haxe.io.Input, to: {value: String}) {
+			to.value = stream.readAll().toString();
+			streamsLock.release();
+		}
 
-			final out = {value: ""};
-			final err = {value: ""};
-			Thread.create(readFrom.bind(p.stdout, out));
-			Thread.create(readFrom.bind(p.stderr, err));
+		final out = {value: ""};
+		final err = {value: ""};
+		Thread.create(readFrom.bind(p.stdout, out));
+		Thread.create(readFrom.bind(p.stderr, err));
 
-			final code = p.exitCode();
-			for (_ in 0...2) {
-				// wait until we finish reading from both streams
-				streamsLock.wait();
-			}
-			{
-				code: code,
-				out: out.value,
-				err: err.value
-			};
-		} else {
-			final out = p.stdout.readAll().toString();
-			final err = p.stderr.readAll().toString();
-			final code = p.exitCode();
-			{
-				code: code,
-				out: out,
-				err: err
-			};
+		final code = p.exitCode();
+		for (_ in 0...2) {
+			// wait until we finish reading from both streams
+			streamsLock.wait();
+		}
+
+		final ret = {
+			code: code,
+			out: out.value,
+			err: err.value
 		};
-
 		p.close();
 		return ret;
 	}
